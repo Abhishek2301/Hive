32da7f7069781cb242b7f25146914355221001d6
Abhishek <abhat002@fiu.edu>
2013-02-13 09:08:25 -0500

    Commit: 02-13-2013 07:16PM

--- src/UDAFTopNPercent.java
+++ src/UDAFTopNPercent.java
@@ -1,70 +1,92 @@
-/* UDAF to return rows by computing the top N percent
- * of a selected column attribute from the table
-*/
-
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.apache.hadoop.hive.ql.exec.UDAF;
 import org.apache.hadoop.hive.ql.exec.UDAFEvaluator;
+import org.apache.hadoop.hive.serde2.io.DoubleWritable;
 
-public class UDAFTopNPercent extends UDAF{
-	
+public class UDAFTopNPercent extends UDAF {
+
 	public static class Result {
-		ArrayList<Double> list;
-		double min;
+		List<Double> list;
+		int numRows;
+		int pct;
 	}
-	
-	public class TopNPercentEvaluator implements UDAFEvaluator {
-		
+
+	public static class TopNPercentEvaluator implements UDAFEvaluator {
+
 		private Result res;
-		private int rowIndex;
-		private int percent;
-		
+
 		public TopNPercentEvaluator() {
 			super();
 			res = new Result();
 			init();
-			rowIndex = 0;
 		}
+
 		@Override
 		public void init() {
 			res.list = new ArrayList<Double>();
-			res.min = Double.MAX_VALUE;
+			res.numRows = 0;
+			res.pct = 0;
 		}
-		
-		public boolean iterate(Double rowVal, int pct) {
-			ArrayList<Double> resList = res.list;
-			rowIndex++;
+
+		public boolean iterate(double rowVal, int pct) {
+			List<Double> resList = res.list;
+			res.numRows++;
 			resList.add(rowVal);
-			percent = pct;
+			res.pct = pct;
 			return true;
 		}
-		
-		public ArrayList<Double> terminatePartial() {
-			ArrayList<Double> resList = res.list;
+
+		public Result terminatePartial() {
+			if (res.list == null) {
+				return null;
+			}
+			if (res.list.size() == 0) {
+				return null;
+			}
+			List<Double> resList = res.list;
 			Collections.sort(resList);
-			return resList;
+			List<DoubleWritable> resultList = new ArrayList<DoubleWritable>();
+			for (int i = 0; i < resList.size(); i++) {
+				resultList.add(i, new DoubleWritable(resList.get(i)));
+			}
+			return res;
 		}
-		
-		public boolean merge(ArrayList<Double> otherList) {
-			ArrayList<Double> resList = res.list;
-			resList.addAll(otherList);
+
+		public boolean merge(Result otherResult) {
+			List<Double> resList = res.list;
+			for (int i = 0; i < otherResult.list.size(); i++) {
+				resList.add(otherResult.list.get(i));
+			}
+			res.numRows += otherResult.numRows;
+			res.pct = Math.max(res.pct, otherResult.pct);
 			return true;
 		}
-		
-		public ArrayList<Double> terminate() {
-			ArrayList<Double> resList = res.list;
-			double num_rows = (double)percent/100.0*rowIndex;
+
+		public List<DoubleWritable> terminate() {
+			List<Double> resList = res.list;
+			List<DoubleWritable> resultList = new ArrayList<DoubleWritable>();
+			int rowIndex = res.numRows;
+			int percent = res.pct;
+			double num_rows = (double) percent / 100.0 * rowIndex;
 			Collections.sort(resList);
-			int lastIdx = resList.size()- (int) num_rows;
-			if(lastIdx <= 0) {
-				return resList;
+			Collections.reverse(resList);
+			int lastIdx = resList.size() - (int) num_rows;
+			if (lastIdx <= 0) {
+				for (int i = 0; i < resList.size(); i++) {
+					resultList.add(i, new DoubleWritable(resList.get(i)));
+				}
+				return resultList;
 			}
-			for(int i=0; i<lastIdx; i++) {
-				resList.remove(i);
+			for (int i = 0; i < lastIdx; i++) {
+				resList.remove(resList.size() - 1);
 			}
-			return resList;
+			for (int i = 0; i < resList.size(); i++) {
+				resultList.add(i, new DoubleWritable(resList.get(i)));
+			}
+			return resultList;
 		}
 	}
 
